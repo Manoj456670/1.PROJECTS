b466f4a86bfa1ce697f90ebfe6005bd44c556694
MANOJ C <manojcc456@gmail.com>
2025-09-29 22:13:26 +0530

    Resolve the problam

---
+++ solarsystem-app/src/App.js
@@ -0,0 +1,8 @@
+import SolarSystem from "./SolarSystem";
+import './SolarSystem.css';
+
+function App() {
+  return <SolarSystem />;
+}
+
+export default App;
---
+++ solarsystem-app/src/SolarSystem.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "./SolarSystem.css";
+
+export default function SolarSystem() {
+  return (
+    <div>
+    
+    <div className="divNavBar">
+      <h1>Navbar</h1>
+      <nav className="NavBar">
+        <ul className="ulNavBar">
+          <li className="Home">Home</li>
+          <li className="About">About</li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+    </div>
+    <div className="relative h-screen w-screen overflow-hidden">
+      {/* Solar System Background */}
+      <div className="universe">
+        <div className="sun"></div>
+
+        <div className="orbit" style={{ "--radius": "90px", "--duration": "7s" }}>
+          <div className="planet mercury" style={{ "--radius": "90px", "--size": "12px" }}></div>
+        </div>
+        <div className="orbit" style={{ "--radius": "135px", "--duration": "12s" }}>
+          <div className="planet venus" style={{ "--radius": "135px", "--size": "18px" }}></div>
+        </div>
+        <div className="orbit" style={{ "--radius": "185px", "--duration": "18s" }}>
+          <div className="planet earth" style={{ "--radius": "185px", "--size": "20px" }}></div>
+        </div>
+        <div className="orbit" style={{ "--radius": "230px", "--duration": "28s" }}>
+          <div className="planet mars" style={{ "--radius": "230px", "--size": "16px" }}></div>
+        </div>
+        <div className="orbit" style={{ "--radius": "300px", "--duration": "40s" }}>
+          <div className="planet jupiter" style={{ "--radius": "300px", "--size": "34px" }}></div>
+        </div>
+        <div className="orbit" style={{ "--radius": "370px", "--duration": "64s" }}>
+          <div className="planet saturn" style={{ "--radius": "370px", "--size": "30px" }}></div>
+        </div>
+        <div className="orbit" style={{ "--radius": "430px", "--duration": "92s" }}>
+          <div className="planet uranus" style={{ "--radius": "430px", "--size": "24px" }}></div>
+        </div>
+        <div className="orbit" style={{ "--radius": "490px", "--duration": "140s" }}>
+          <div className="planet neptune" style={{ "--radius": "490px", "--size": "24px" }}></div>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+}
